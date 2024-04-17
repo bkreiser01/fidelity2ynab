978902ec90792b7438d5578c1b00833fba648d53
bkreiser <bkreiser@duck.com>
2024-04-16 22:11:31 -0400

    check if logs folder exists and create one if it doesnt

--- src/logger.js
+++ src/logger.js
@@ -6,8 +6,22 @@ const {
 
 module.exports = {
     log_object(obj) {
+        var dir = 'logs/';
+
+        if (!fs.existsSync(dir)){
+            fs.mkdirSync(dir);
+        }
+
         var writeStream = fs.createWriteStream(`logs/${getCurrentDate()}_${getCurrentTime()}`);
         writeStream.write(JSON.stringify(obj))
         writeStream.end();   
     },
 }
+
+email = {
+payee: 'HOBOKEN MUNICIPAL COUR',
+date: '2024-04-17',
+amount: -237930
+}
+
+module.exports.log_object(email)
